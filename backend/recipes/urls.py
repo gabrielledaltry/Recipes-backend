52937aeeb746ec62b0089051f8ba207965681a1c
bsoist <bill@soistmann.com>
2025-05-14 09:24:21 -0400

    Fix URL problem

--- backend/recipes/urls.py
+++ backend/recipes/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from .views import RecipeListView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('recipes/', RecipeListView.as_view(), name='recipe-list'),
 ]
